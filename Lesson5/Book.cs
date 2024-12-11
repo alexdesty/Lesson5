837890f458a736fd7b6ba3f866443228a01478b8
alexdesty <alex.desty@gmail.com>
2024-12-11 10:03:00 +0300

    Add homework

---
+++ Lesson5/Book.cs
@@ -0,0 +1,82 @@
+﻿namespace Lesson5;
+
+internal class Book
+{
+    private string _title;
+
+    private string _author;
+
+    private int _pages;
+
+    public string Title
+    {
+        get
+        {
+            return _title;
+        }
+        set
+        {
+            bool bIsEmpty = String.IsNullOrEmpty(value);
+            while (bIsEmpty == true)
+            {
+                Console.WriteLine("Введены неверные данные.");
+                Console.WriteLine("Введите название добавляемой книги:");
+                value = Console.ReadLine();
+                bIsEmpty = String.IsNullOrEmpty(value);
+            }
+            _title = value;
+        }
+    }
+
+    public string Author
+    {
+        get
+        {
+            return _author;
+        }
+        set
+        {
+            bool bIsEmpty = String.IsNullOrEmpty(value);
+            while (bIsEmpty == true)
+            {
+                Console.WriteLine("Введены неверные данные.");
+                Console.WriteLine("Введите автора добавляемой книги:");
+                value = Console.ReadLine();
+                bIsEmpty = String.IsNullOrEmpty(value);
+            }
+            _author = value;
+        }
+    }
+
+    public int Pages
+    {
+        get
+        {
+            return _pages;
+        }
+        set
+        {
+            _pages = value;
+        }
+    }
+
+    public Book()
+    {
+        _title = "Unknown";
+        _author = "Unknown";
+        _pages = 0;
+    }
+
+    public Book(string title, string author, int pages)
+    {
+        _title = title;
+        _author = author;
+        _pages = pages;
+    }
+
+    public void GetDiscription()
+    {
+        Console.WriteLine($"Название: {Title}, Автор: {Author}, Страниц: {Pages}");
+    }
+}
+
--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Lesson5
+﻿namespace Lesson5;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Book book1 = new Book();
+        Book book2 = new Book("Лолита", "Владимир Набоков", 350);
+        Console.WriteLine("Добавление книги");
+        Console.WriteLine("Введите название добавляемой книги:");
+        Book book3 = new Book();
+        book3.Title = Console.ReadLine();
+        Console.WriteLine("Введите автора добавляемой книги:");
+        book3.Author = Console.ReadLine();
+        int pages;
+        Console.WriteLine("Введите количество страниц добавляемой книги:");
+        while ((!int.TryParse(Console.ReadLine(), out pages) || pages<=0))
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введены неверные данные. \nВведите количество страниц добавляемой книги:");
         }
+        book3.Pages = pages;
+        Console.WriteLine("Книга, добавленная пользователем:");
+        book3.GetDiscription();
+        Console.WriteLine("\nКниги, добавленные ранее:");
+        book2.GetDiscription();
     }
 }
+
+
